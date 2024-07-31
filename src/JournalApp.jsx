f4c60b82dbffb63b60a3383b1ca4400c01be6688
salcrom <seya.28@gmail.com>
2024-07-31 17:19:14 +0200

    creadas rutas publicas y privadas

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import { AppRouter } from "./router/AppRouter";
 
 export const JournalApp = () => {
     return (
         <>
-            <h1>Journal App</h1>
+            <AppRouter />
         </>
     );
 };
